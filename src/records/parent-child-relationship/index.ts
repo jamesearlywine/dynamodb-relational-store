7cd7b4d6b61a0194d0b3008ebdb1e4fc0b262b50
James Earlywine <james@esilogix.com>
2026-03-06 12:57:04 -0500

    ..

--- src/records/parent-child-relationship/index.ts
+++ src/records/parent-child-relationship/index.ts
@@ -2,4 +2,3 @@ export * from './parent-child-relationship-record.factory';
 export * from './parent-child-relationship-record.type';
 export * from './parent-child-relationship-record.schema';
 export * from './parent-child-relationship-record.typeguard';
-export * from './parent-child-relationship-record.test';
